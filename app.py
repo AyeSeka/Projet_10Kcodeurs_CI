5150394e77522ebd9855caab1e0fdf641d4aa5c2
AyeSeka <ayeseka225@gmail.com>
2025-05-23 16:59:50 +0000

    Page index OKAY

---
+++ app.py
@@ -0,0 +1,17 @@
+#installation librairie
+from flask import Flask,render_template
+
+
+#debut app
+app = Flask(__name__)
+
+
+#index
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
